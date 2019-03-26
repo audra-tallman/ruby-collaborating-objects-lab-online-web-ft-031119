7aa743174bd8ff8afe342a38a9d4c1fb617b3d8e
Audra Tallman <audi.tallman@gmail.com>
2019-03-26 18:58:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+ attr_accessor :name, :artist
+ 
+  def initialize(name)
+    @name= name
+  end
+  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+  
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+  
+end 
