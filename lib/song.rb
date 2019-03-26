6f21a02055112cfdb25c3d542e7210a094de78f8
Audra Tallman <audi.tallman@gmail.com>
2019-03-26 19:42:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
     new_song
   end
   
-   def artist_name=(name)
-      self.artist = Artist.find_or_create_by_name(name)
-      artist.add_song(self)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   
 end 
